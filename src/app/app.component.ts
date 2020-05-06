9cd9ebad52b9aab66ae0a31d370f399bc2dbacb8
tamasc <tamas.czerjak@gmail.com>
2020-05-06 19:53:56 +0200

    start implementing undo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'undo-visualiser';
+
+  transaction = '';
+  ownTransaction = '';
+
+  snapshot = [];
+  transactionQueue = [];
+  undoQueue = [];
+  redoQueue = [];
+
+  ownTransactions = [];
+
+  addTransaction() {
+    this.transactionQueue.push('___' + this.transaction);
+    this.transaction = '';
+    this.ownTransactions = [...this.transactionQueue].reverse();
+  }
+
+  addOwnTransaction() {
+    const ownTransaction = '_x_' + this.ownTransaction;
+    this.transactionQueue.push(ownTransaction);
+    this.ownTransaction = '';
+    this.ownTransactions = [...this.transactionQueue].reverse();
+  }
+
+  addUndo() {
+    const lastOwnTransaction = this.ownTransactions[this.ownTransactions.length - 1];
+    const undoTransaction = 'u' + lastOwnTransaction.slice(1);
+    this.undoQueue.push(undoTransaction);
+    this.transactionQueue.push(undoTransaction);
+  }
+
+  addRedo() {
+    // this.redoQueue.push(this.redo);
+  }
 }
